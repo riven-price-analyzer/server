dcb17aca17d5df0086e7db46b1d215f8d06641a5
Piotr-Aueternum <piotr.aueternum@gmail.com>
2019-07-07 23:18:17 +0200

    Setup paperplane's server

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('hello');
+import R from 'ramda'
+import http from 'http'
+import paperlane from 'paperplane'
+const { json, methods, mount, routes } = paperlane;
+
+
+const logReqType = req => console.log(`${req.method} ${req.url}`);
+
+const logReq = R.tap(R.pipe(R.prop('body'), console.log));
+
+const logger = 
+  R.tap(
+    R.pipe(
+      R.when(R.is(Error), R.pick(['message', 'name', 'stack'])),
+      R.tap(R.pipe(
+        R.prop('req'),
+        logReqType,
+      )),
+      R.evolve({
+        req: R.pick(['headers', 'method', 'url']),
+        res: R.pick(['statusCode'])
+      }),
+      data => JSON.stringify(data, null, 2),
+      console.info,
+    )
+  )
+
+const app = routes({
+  '/stats/:name': methods({
+    POST: R.pipe(logReq, R.prop('body'), json),
+  }),
+  '*': methods({
+    GET: hello
+  }),
+});
+ 
+http
+    .createServer(mount({ app, logger }))
+    .listen(3000, logger);
